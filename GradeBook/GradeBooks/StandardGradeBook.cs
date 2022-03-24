140933e5a222b34ba9b914681e51780ced6e4f6d
Miłosz Robak <milosz@wandlee.com>
2022-03-24 19:29:12 +0100

    make BaseGradeBook class abstract, Add IsWeighted property

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(String name) : base(name)
+        public StandardGradeBook(String name, bool isWeighted) : base(name, isWeighted)
         {
             Type = Enums.GradeBookType.Standard;
         }
